d9f29e27904f494175362cb897a0e1b572d7c632
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 23:43:38 +0530

    ..

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter email");
+            String email = Console.ReadLine();// input is : ramtej5524@gmail.com
+            Console.WriteLine("enter name");
+            String name = Console.ReadLine();
+            Console.WriteLine("enter DOB");
+            String dob = Console.ReadLine();
+
+
+            String pattern = "[a-z]{1,9}[0-9]{0,9}[@]{1}[a-z]{1,7}[.]{1}[a-z]{2,3}";
+            string pattern1 = "[A-Z][a-z]";
+            string pattern2 = "[0-9]{1,2}[/]{1}[0-9]{1,2}[/]{1}[0-9]{4}";
+            bool b = Regex.IsMatch(email, pattern);
+            bool b1 = Regex.IsMatch(name, pattern1);
+            bool b2 = Regex.IsMatch(dob, pattern2);
+            Console.WriteLine(b);
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            
+        }
+    }
+}
